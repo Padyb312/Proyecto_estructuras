548e70524bbe2360176f901871ac63a0a73dbd16
juanj <juanj@Pady_V2>
2026-04-16 14:36:01 -0500

    Coreeccion 2

--- Stockfish/src/modelo/ActividadPersonal.java
+++ Stockfish/src/modelo/ActividadPersonal.java
@@ -1,8 +1,8 @@
 package modelo;
 
-public class ActividadPersonal extends Evento {
-	
-	private static final long serialVersionUID = 1L;
+import java.io.Serializable;
+
+public class ActividadPersonal extends Evento implements Serializable {
 
 	public ActividadPersonal(String fecha, String actividad, int prioridad, boolean estado, int franja) {
 		super(fecha, actividad, prioridad, estado, franja);
